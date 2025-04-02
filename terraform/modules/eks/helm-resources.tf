65c5a3d8a02a0a7009ad3959b2ecfa3525247efc
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2025-04-02 12:45:41 +0400

    Update helm-resources.tf

--- terraform/modules/eks/helm-resources.tf
+++ terraform/modules/eks/helm-resources.tf
@@ -17,7 +17,7 @@ data "aws_eks_cluster_auth" "this" {
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(var.cluster_ca_cert)
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--output", "json"]
