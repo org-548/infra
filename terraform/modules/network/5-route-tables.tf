d7567f43276fade3d54920786916fcd7d0521618
Tereza <bisharyant@gmail.com>
2025-04-07 12:31:59 +0400

    Ignore rt for prib-subnet

--- terraform/modules/network/5-route-tables.tf
+++ terraform/modules/network/5-route-tables.tf
@@ -11,19 +11,19 @@ resource "aws_route_table" "for_pub_sub" {
   }
 }
 
-resource "aws_route_table" "for_priv_sub" {
-  count  = var.priv_rt_cnt
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block     = var.default_gateway
-    nat_gateway_id = element(aws_nat_gateway.nat.*.id, count.index)
-  }
-
-  tags = {
-    name = var.rt_priv_tag
-  }
-}
+#resource "aws_route_table" "for_priv_sub" {
+#  count  = var.priv_rt_cnt
+#  vpc_id = aws_vpc.vpc.id
+#
+#  route {
+#    cidr_block     = var.default_gateway
+#    nat_gateway_id = element(aws_nat_gateway.nat.*.id, count.index)
+#  }
+#
+#  tags = {
+#    name = var.rt_priv_tag
+#  }
+#}
 
 resource "aws_route_table_association" "pub_sub_assoc" {
   count          = var.pub_sub_cnt
@@ -31,8 +31,8 @@ resource "aws_route_table_association" "pub_sub_assoc" {
   route_table_id = aws_route_table.for_pub_sub.id
 }
 
-resource "aws_route_table_association" "priv_sub_assoc" {
-  count          = var.priv_sub_cnt
-  subnet_id      = element(aws_subnet.priv[*].id, count.index)
-  route_table_id = aws_route_table.for_priv_sub[count.index].id
-}
+#resource "aws_route_table_association" "priv_sub_assoc" {
+#  count          = var.priv_sub_cnt
+#  subnet_id      = element(aws_subnet.priv[*].id, count.index)
+#  route_table_id = aws_route_table.for_priv_sub[count.index].id
+#}
