9ee4c951756b02cf599e8e731983d401d10eaed7
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2025-04-07 12:25:23 +0400

    Ignore private-subnet tf resource

--- terraform/modules/network/3-subnets.tf
+++ terraform/modules/network/3-subnets.tf
@@ -15,19 +15,19 @@ resource "aws_subnet" "pub" {
   }
 }
 
-resource "aws_subnet" "priv" {
-  count             = var.priv_sub_cnt
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 8, var.priv_sub_cnt + count.index)
-  availability_zone = data.aws_availability_zones.az.names[count.index]
-  ipv6_native       = var.only_ipv6
+#resource "aws_subnet" "priv" {
+  #count             = var.priv_sub_cnt
+  #vpc_id            = aws_vpc.vpc.id
+  #cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 8, var.priv_sub_cnt + count.index)
+  #availability_zone = data.aws_availability_zones.az.names[count.index]
+  #ipv6_native       = var.only_ipv6
 
-  tags = {
-    Name                              = "${var.priv_sub_tag}-${count.index + 1}"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetis.io/cluster/first-eks"      = "owned"
-  }
-}
+  #tags = {
+    #Name                              = "${var.priv_sub_tag}-${count.index + 1}"
+    #"kubernetes.io/role/internal-elb" = "1"
+    #"kubernetis.io/cluster/first-eks"      = "owned"
+  #}
+#}
 
 data "aws_subnets" "pub" {
   count = var.subnets_data_cnt
