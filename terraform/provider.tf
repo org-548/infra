ed8ebad1c25cde801721e8659e509edbabe73949
Tereza <bisharyant@gmail.com>
2025-05-06 05:59:43 +0400

    Back to default tf-backend conf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,13 @@
-terraform {
-  backend "s3" {
-    bucket = "tf-backend-f"
-    key    = "terraform/backend/tfstate"
-    region = "eu-north-1"
-  }
-}
-
-#provider "aws" {
-#  shared_config_files      = ["~/.aws/config"]
-#  shared_credentials_files = ["~/.aws/credentials"]
-#  #profile = "default"
+#terraform {
+#  backend "s3" {
+#    bucket = "tf-backend-f"
+#    key    = "terraform/backend/tfstate"
+#    region = "eu-north-1"
+#  }
 #}
+
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  #profile = "default"
+}
